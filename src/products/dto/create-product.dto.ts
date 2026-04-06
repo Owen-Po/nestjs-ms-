a8a554f070305e772774c72d1a8a9e81199e9717
Owen-Po <owen.pinedo@hannahlab.com>
2026-04-06 11:08:15 -0500

    Feat: Implementando el dto, agregando las validaciones , esto es los datos que vienen desde el cliente.

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,5 +1,13 @@
+import { IsNumber, IsString, Min } from 'class-validator';
+import { Type } from 'class-transformer';
 export class CreateProductDto {
+  @IsString()
   public name: string;
 
-  public price: number;
+  @IsNumber({
+    maxDecimalPlaces: 4,
+  })
+  @Min(0)
+  @Type(() => Number)
+  public price: string;
 }
